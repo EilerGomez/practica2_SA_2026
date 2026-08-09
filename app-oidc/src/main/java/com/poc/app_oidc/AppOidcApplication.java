35c71925a19afb7a84735e01d6bb7284c30b2ffc
EilerGomez <gomezeiler250@gmail.com>
2026-08-08 21:37:28 -0600

    agregando backend del poc oidc Oauth

---
+++ app-oidc/src/main/java/com/poc/app_oidc/AppOidcApplication.java
@@ -0,0 +1,13 @@
+package com.poc.app_oidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOidcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOidcApplication.class, args);
+	}
+
+}
---
+++ app-oidc/src/test/java/com/poc/app_oidc/AppOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.app_oidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
